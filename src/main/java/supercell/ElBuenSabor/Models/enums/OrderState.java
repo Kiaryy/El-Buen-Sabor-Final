5c7d3c619b981dd7aff5727cbedae89a2b3c9610
Augusto <otsuguariquelme@gmail.com>
2025-06-27 00:21:47 -0300

    arreglo de estado de la orden

--- src/main/java/supercell/ElBuenSabor/Models/enums/OrderState.java
+++ src/main/java/supercell/ElBuenSabor/Models/enums/OrderState.java
@@ -1,5 +1,5 @@
 package supercell.ElBuenSabor.Models.enums;
 
 public enum OrderState {
-    PREPARING, PENDING, CANCELED, REJECTED, ARRIVED
+    PREPARING, PENDING, CANCELED, REJECTED, ARRIVED,BILLED, READY_FOR_DELIVERY, ON_THE_WAY
 }
