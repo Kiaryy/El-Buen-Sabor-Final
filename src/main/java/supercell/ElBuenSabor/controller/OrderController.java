80665c5aaea29c5d87104b5e1fb985d82fff90c9
Kiaryy <martin360.moron@gmail.com>
2025-07-13 16:20:37 -0300

    MP hotfix

--- src/main/java/supercell/ElBuenSabor/controller/OrderController.java
+++ src/main/java/supercell/ElBuenSabor/controller/OrderController.java
@@ -21,6 +21,7 @@ import supercell.ElBuenSabor.service.OrderService;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/orders")
@@ -71,24 +72,23 @@ public class OrderController {
     }
 
     @PostMapping("/createPreference")
-    public Preference createPreference(@RequestBody List<UserPreferenceRequest> userItemsPreference ) {
+    public ResponseEntity<Map<String, String>> createPreference(@RequestBody List<UserPreferenceRequest> userItemsPreference) {
         MercadoPagoConfig.setAccessToken("APP_USR-5808544844698208-051510-6264c6c45bd16af4b067e869f6cb9357-2439047893");
 
         List<PreferenceItemRequest> items = new ArrayList<>();
 
         userItemsPreference.forEach(userPreference -> {
             PreferenceItemRequest itemRequest = PreferenceItemRequest.builder()
-                    .id("1234") //require
-                    .title(userPreference.title()) //require
+                    .id("1234") // required
+                    .title(userPreference.title())
                     .pictureUrl("http://picture.com/PS5")
                     .categoryId("games")
-                    .quantity(userPreference.quantity()) //require
-                    .unitPrice(new BigDecimal(userPreference.price())) //require
+                    .quantity(userPreference.quantity())
+                    .unitPrice(new BigDecimal(userPreference.price()))
                     .build();
             items.add(itemRequest);
         });
 
-        // Preference Back url
         PreferenceBackUrlsRequest backUrl = PreferenceBackUrlsRequest.builder()
                 .success("http://127.0.0.1:5501/front-end/menu.html")
                 .pending("http://127.0.0.1:5501/front-end/menu.html")
@@ -97,19 +97,17 @@ public class OrderController {
 
         PreferenceRequest preferenceRequest = PreferenceRequest.builder()
                 .backUrls(backUrl)
-                .items(items).build();
+                .items(items)
+                .build();
 
-        PreferenceClient client = new PreferenceClient();
-        Preference preference;
         try {
-            preference = client.create(preferenceRequest);
-
-        } catch (MPException e) {
-            throw new RuntimeException(e);
-        } catch (MPApiException e) {
-            throw new RuntimeException(e);
+            PreferenceClient client = new PreferenceClient();
+            Preference preference = client.create(preferenceRequest);
+            log.info("Created MercadoPago Preference: " + preference.getId());
+            return ResponseEntity.ok(Map.of("init_point", preference.getInitPoint()));
+        } catch (MPException | MPApiException e) {
+            throw new RuntimeException("Error creating MercadoPago preference", e);
         }
-        log.info(preference);
-        return preference;
     }
+
 }
